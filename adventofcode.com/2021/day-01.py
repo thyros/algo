3380d96a94a677926da60f4f69a556ee41589f20
robert.starzycki <robert.starzycki@king.com>
2021-12-04 16:32:39 +0000

    day 1, 2

---
+++ adventofcode.com/2021/day-01.py
@@ -0,0 +1,18 @@
+from typing import DefaultDict
+
+def part_1(data):
+    diffs = [j - i for i, j in zip(data, data[1:])]
+    increased = sum(1 for x in diffs if x > 0)
+    return increased
+
+def part_2(data):
+    sums = [a+b+c for a, b, c in zip(data, data[1:], data[2:])]
+    diffs = [j - i for i, j in zip(sums, sums[1:])]
+    increased = sum(1 for x in diffs if x > 0)
+    return increased
+
+with open("day-01.input", "r") as fin:
+    DATA = [int(x) for x in fin.readlines()]
+
+print("Part 1: {}".format(part_1(DATA)))
+print("Part 2: {}".format(part_2(DATA)))
