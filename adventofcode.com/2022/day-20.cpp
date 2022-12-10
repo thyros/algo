2d58af18376c99356e7cefcae6c0852daf9b8ff5
Robert Starzycki <robert.starzycki@king.com>
2022-12-10 10:29:46 +0000

    add templates for remaining days

---
+++ adventofcode.com/2022/day-20.cpp
@@ -0,0 +1,22 @@
+#include "utils.h"
+#include <cassert>
+#include <cstdio>
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+
+
+void solve(const char* filename) {
+
+}
+
+void test() {
+
+}
+
+int main() {
+    test();
+
+    constexpr char filename[] { "day-20.sample" };
+    solve(filename);
+}
