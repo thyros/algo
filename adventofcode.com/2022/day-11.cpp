ab4996b2858632a4ac5617b524bbb5a5c108433e
Robert Starzycki <robert.starzycki@king.com>
2022-12-11 20:22:34 +0000

    starting day 11

--- adventofcode.com/2022/day-11.cpp
+++ adventofcode.com/2022/day-11.cpp
@@ -4,9 +4,29 @@
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
+
+struct Monkey {
+    std::vector<int> items;
+
+    std::function<int(int)> op;
+    int div;
+
+    int trueMonkey;
+    int falseMonkey;
+};
+using Monkeys = std::vector<Monkey>;
+
+Monkeys toMonkeys(const Lines& lines) {
+    Monkeys result;
+
+    return result;
+}
 
 void solve(const char* filename) {
+    const Lines lines = readFile(filename);
+
 
 }
 
