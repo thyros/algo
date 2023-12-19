9eae64f6c2bcfb25e40be848a931e8bd9d39cede
Robert Starzycki <robert.starzycki@king.com>
2023-12-19 21:17:28 +0000

    day10 wip

--- adventofcode.com/2023/day10.cpp
+++ adventofcode.com/2023/day10.cpp
@@ -12,7 +12,7 @@ Offsets getOffsetsForStartingPoints(const Lines& lines, const size_t height, con
     int currentOffset = 0;
     if (start.y > 0) {
         const char c = lines[start.y - 1][start.x];
-        if (c == '|' || c == 'L' || c == 'J') {
+        if (c == '|' || c == 'F' || c == '7') {
             offsets[currentOffset++] = Point {0, -1};
         }
     }
@@ -24,31 +24,45 @@ Offsets getOffsetsForStartingPoints(const Lines& lines, const size_t height, con
     }
     if (start.y < height - 1) {
         const char c = lines[start.y + 1][start.x];
-        if (c == '|' || c == 'F' || c == 'J') {
+        if (c == '|' || c == 'L' || c == 'J') {
             offsets[currentOffset++] = Point {0, 1};
         }
     }
     if (start.x < width - 1) {
         const char c = lines[start.y][start.x + 1];
-        if (c == '-' || c == 'L' || c == 'F') {
+        if (c == '-' || c == '7' || c == 'J') {
             offsets[currentOffset++] = Point {1, 0};
         }
     }
     return offsets;
 }
 
-Offsets getOffsetForChar(const char c) {
+Offsets getOffsetsForChar(const char c) {
     if (c == '|') return { Point {0, -1}, Point {0, 1} };
     if (c == '-') return { Point {-1, 0}, Point {1, 0} };
     if (c == 'L') return { Point {0, -1}, Point {1, 0} };
-    if (c == 'J') return { Point {0, -1}, Point {1, 0} };
-    if (c == 'F') return { Point {0,  1}, Point {0, 1} };
+    if (c == 'J') return { Point {0, -1}, Point {-1, 0} };
+    if (c == 'F') return { Point {0,  1}, Point {1, 0} };
     if (c == '7') return { Point {0,  1}, Point {-1, 0} };
 
     return { Point {0,0}, Point {0,0} };
 }
 
-bool move(Point& pos, Point& offset, std::vector<std::vector<int>>& network) {
+bool move(const Lines& lines, std::vector<std::vector<int>>& network, Point& pos, Point& offset) {
+    const Point nextPoint { pos.x + offset.x, pos.y + offset.y };
+    const char c = lines[nextPoint.y][nextPoint.x];
+
+    if (network[nextPoint.y][nextPoint.x] != 0) {
+        return true;
+    }
+
+    const Offsets nextOffsets = getOffsetsForChar(c);
+    const int chosenOffset = (offset.x == -nextOffsets[0].x && offset.y == -nextOffsets[0].y) ? 1 : 0;
+
+    network[nextPoint.y][nextPoint.x] = network[pos.y][pos.x] + 1;
+    offset = nextOffsets[chosenOffset];
+    pos = nextPoint;
+
     return false;
 }
 
@@ -58,13 +72,15 @@ int solve(const Lines& lines, Point start) {
     std::vector<std::vector<int>> network(height, std::vector<int>(width, 0));
 
     std::array<Point, 2> pos {start, start};
+    Offsets offset = getOffsetsForStartingPoints(lines, height, width, start);
 
     while (true) {
-        if (move(pos[0], offset[0], network)) {
-            return network.at(pos[0].y + offset[0].y]).at([pos[0].x + offset[0].x]);
+        for (int i = 0; i <= 1; ++i) {
+            if (move(lines, network, pos[i], offset[i])) {
+                return network[pos[i].y + offset[i].y][pos[i].x + offset[1].x];
+            }
         }
     }
-
 
     return 0;
 }
@@ -84,9 +100,10 @@ void solve(const char* filename) {
         }
     }
 
-    const int result = solve(lines, start);    
+    const int result = solve(lines, start); 
     std::cout << std::format("Longest point can be reached in {} steps\n", result);
 }
+
 
 void test() {
     using namespace boost::ut;
@@ -98,6 +115,6 @@ void test() {
 
 int main(int argc, const char** argv) {
     test();
-    const char* filename = argc > 1 ? argv[1] : "day10.sample";
+    const char* filename = argc > 1 ? argv[1] : "day10.sample2";
     solve(filename);
 }
