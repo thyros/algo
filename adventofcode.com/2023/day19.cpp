ad9e76d6a92420e510a474ce38dc09ab4540e460
Robert Starzycki <robert.starzycki@king.com>
2024-01-10 06:50:33 +0000

    day19 wip

--- adventofcode.com/2023/day19.cpp
+++ adventofcode.com/2023/day19.cpp
@@ -9,7 +9,7 @@ using T = long long;
 struct Condition {
     char param;
     T value;
-    std::function<bool(T, T)> f;
+    char op;
     std::string id;
 };
 
@@ -28,26 +28,34 @@ struct Part {
 };
 using Parts = std::vector<Part>;
 
+enum class PartResult {
+    unknown,
+    rejected,
+    accepted
+};
+
+struct PartRange{
+    std::pair<T,T>x;
+    std::pair<T,T> m;
+    std::pair<T,T> a;
+    std::pair<T,T> s;
+    PartResult result;
+};
+using PartRanges = std::vector<PartRange>;
+
 Workflow parseWorkflow(const std::string& line) {
     const Tokens tokens = tokenize(line, "{");
-    const std::string id {tokens[0]};
+    const std::string id {tokens[1]};
 
     const Tokens conditionTokens = tokenize(tokens[1], ",");
     std::vector<Condition> conditions;
     for (T i = 0; i < conditionTokens.size() - 1; ++i) {
         const std::string_view t = conditionTokens[i];
-        std::function<bool(T, T)> f;
-        if (t[1] == '>') {
-            f = std::greater<T>();
-        } else {
-            f = std::less<T>();
-        }
-        
         const size_t colon = t.find(':');
         const T value{std::stoll(toString(t.substr(2, colon - 2)))};
         const std::string defaultWorkflow{t.substr(colon + 1)};
 
-        conditions.push_back(Condition {.param = t[0], .value = value, .f = f, .id = defaultWorkflow});
+        conditions.push_back(Condition {.param = t[0], .value = value, .op = t[1], .id = defaultWorkflow});
     }
 
     // the last condition is a default workflow. Use it directly but without the '}'
@@ -94,7 +102,13 @@ const T& at(const Part& part, char param) {
     return part.s;
 }
 
-bool simulate(const Workflows& workflows, const Part& part, std::string workflow) {
+bool accept(const Workflows& workflows, const Part& part, std::string workflow) {
+    const auto op = [](char o, T lhs, T rhs) {
+        if (o == '>') {
+            return std::greater<T>()(lhs, rhs);
+        }
+        return std::less<T>()(lhs, rhs);
+    };
     while (1) {
         if (workflow == "A") {
             return true;
@@ -107,7 +121,7 @@ bool simulate(const Workflows& workflows, const Part& part, std::string workflow
         bool handled = false;
         for (const Condition& c: w.conditions) {
             const T& paramValue = at(part, c.param);
-            if (c.f(paramValue, c.value)) {
+            if (op(c.op, paramValue, c.value)) {
                 workflow = c.id;
                 handled = true;
                 break;
@@ -125,7 +139,7 @@ void solvePart1(const Workflows& workflows, const Parts& parts, const std::strin
     T total = 0;
 
     for (const Part& p: parts) {
-        if (simulate(workflows, p, "in")) {
+        if (accept(workflows, p, workflow)) {
             const T partTotal = p.x + p.m + p.a + p.s;
             total += partTotal;
 
@@ -134,6 +148,16 @@ void solvePart1(const Workflows& workflows, const Parts& parts, const std::strin
     }
 
     std::cout << std::format("part 1: {}\n", total);
+}
+
+void solvePart2(const Workflows& workflows, std::string inWorkflow) {
+    PartRanges ranges { PartRange {{0, 4000}, {0, 4000}, {0, 4000}, {0, 4000}}};
+
+    while (1) {
+        PartRanges nextRanges;
+
+
+    }
 }
 
 void solve(const char* filename) {
@@ -166,6 +190,29 @@ void test() {
         expect(part.a == 1222_i);
         expect(part.s == 2876_i);
     };
+
+    // "splitting a single range"_test = [] {
+    //     PartRanges nextRanges = split(PartRange{{0, 4000}, {0, 4000}, {0, 4000}, {0, 4000}}, 'x', '<', 1000);
+
+    //     expect(nextRanges.size() == 2_i);
+    //     expect(nextRanges[0].x.first == 0_i);
+    //     expect(nextRanges[0].x.second == 999_i);
+    //     expect(nextRanges[0].m.first == 0_i);
+    //     expect(nextRanges[0].m.second == 4000_i);
+    //     expect(nextRanges[0].a.first == 0_i);
+    //     expect(nextRanges[0].a.second == 4000_i);
+    //     expect(nextRanges[0].s.first == 0_i);
+    //     expect(nextRanges[0].s.second == 4000_i);
+
+    //     expect(nextRanges[1].x.first == 0_i);
+    //     expect(nextRanges[1].x.second == 1000_i);
+    //     expect(nextRanges[1].m.first == 0_i);
+    //     expect(nextRanges[1].m.second == 4000_i);
+    //     expect(nextRanges[1].a.first == 0_i);
+    //     expect(nextRanges[1].a.second == 4000_i);
+    //     expect(nextRanges[1].s.first == 0_i);
+    //     expect(nextRanges[1].s.second == 4000_i);
+    // };
 }
 
 int main(int argc, const char** argv) {
