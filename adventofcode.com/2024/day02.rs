12bbfa8a17d6ceca9612567b806a95a24175aba9
Thyros <thyros@gmail.com>
2024-12-05 21:14:57 +0000

    day 2

--- adventofcode.com/2024/day02.rs
+++ adventofcode.com/2024/day02.rs
@@ -1,3 +1,70 @@
+use nom::multi::separated_list1;
+
+enum Safety {
+    Safe,
+    Unsafe,
+};
+
+enum Direction {
+    Increasing,
+    Decreasing,
+};
+
+type Report = Vec<i32>;
+
+fn parser(input: &str) -> IResult<&str, Report> {
+    separated_list1(
+        newline,
+        separated_list1(space1, complete::i32),
+    )(input)
+}
+
+fn check_safety(report: Report) -> Safety {
+    let mut direction: Option<Direction> = None;
+
+    for (l, r) in report.iter().tuple_windows() {
+        let diff = l - r;
+        match diff.signum() {
+            -1 => match direction {
+                Some(Direction::Increasing) => {
+                    return Safety::Unsafe;
+                }
+                Some(Direction::Decreasing) => {
+                    if !(1..3).contains(&diff.abs()) {
+                        return Safety::Unsafe;
+                    } else {
+                        continue;
+                    }
+                }
+                None => {
+                    direction = Some(Direction::Increasing);
+                }
+            }
+            1 => match direction {
+                Some(Direction::Decreasing) => {
+                    if !(1..3).contains(&diff.abs()) {
+                        return Safety::Unsafe;
+                    } else {
+                        continue;
+                    }
+                }
+                Some(Direction::Increasing) => {
+                    return Safety::Unsafe;
+                }
+                None => {
+                    direction = Some(Direction::Decreasing);
+                }
+            }
+            0 => {
+                return Safety::Unsafe;
+            }
+        }
+    }
+
+    return Safety::Safe;
+}
+
+
 fn solve1(input: &str) -> i32 {
     let mut total: i32 = 0;
     let mut count: i32 = 0;
