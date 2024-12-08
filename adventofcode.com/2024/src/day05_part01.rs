1ee7635c9b13151a4ec8ac91d917ad4816284385
Thyros <thyros@gmail.com>
2024-12-08 22:16:37 +0000

    day 5

--- adventofcode.com/2024/src/day05_part01.rs
+++ adventofcode.com/2024/src/day05_part01.rs
@@ -1,26 +1,13 @@
 use super::validate;
 
 use itertools::Itertools;
-use std::collections::HashMap;
 use nom::{
-    bytes::complete::tag, character::complete::{self, line_ending}, multi::{fold_many1, separated_list1}, sequence::{separated_pair, terminated}, IResult
+    bytes::complete::tag, character::complete::{self, line_ending}, multi::separated_list1, IResult
 };
 
-fn rules(input: &str) -> IResult<&str, HashMap<u32, Vec<u32>>> {
-    fold_many1(
-        terminated(
-            separated_pair(
-                complete::u32, 
-                tag("|"), 
-                complete::u32),
-            line_ending),
-        HashMap::default,
-        |mut acc: HashMap<u32, Vec<u32>>, (page, after)| {
-            acc.entry(page).and_modify(|afters| {
-                afters.push(after)
-            }).or_insert(vec![after]);
-            acc
-        },
+fn rules(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
+    separated_list1(line_ending, 
+        separated_list1(tag("|"), complete::u32),
     )(input)
 }
 
@@ -30,34 +17,55 @@ fn updates(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
     )(input)
 }
 
-fn parse(input: &str) -> IResult<&str, (HashMap<u32, Vec<u32>>, Vec<Vec<u32>>)> {
-    let (input, parsed_rules) = terminated(rules, line_ending)(input)?;
-    let (input, parsed_updates) = updates(input)?;
+fn parse(input: &str) -> IResult<&str, (Vec<Vec<u32>>, Vec<Vec<u32>>)> {
+    let (input, parsed_rules) = rules(input)?;
+    let (input, parsed_updates) = updates(input.trim_start())?;
+
     Ok((input, (parsed_rules, parsed_updates)))
 }
 
-fn solve(input: &str) -> usize {
+fn valid_update(rules: &Vec<Vec<u32>>, update: &Vec<u32>) -> bool {
+    let invalid = rules
+        .iter()
+        .any(|rule| {
+            let l = rule.get(0).unwrap();
+            let r = rule.get(1).unwrap();
+
+            if !update.contains(l) || !update.contains(r) {
+                return false;
+            }
+
+            let left_index = update.iter().position(|v| v == l).unwrap();
+            let right_index = update.iter().position(|v| v == r).unwrap();
+
+            return left_index >= right_index;
+        });
+
+    return !invalid;
+}
+
+fn solve(input: &str) -> u32 {
+
     let (_input, (parsed_rules, parsed_updates)) = parse(input).unwrap();
 
-    dbg!(&parsed_rules);
-    dbg!(&parsed_updates);
-
-    let results: Vec<usize> = updates
+    let valid_updates = parsed_updates
         .iter()
-        .enumerate()
-        .filter_map(|(index, update)| {
+        .filter(|u| valid_update(&parsed_rules, &u))
+        .collect_vec();
 
-        })
-        .collect();
 
-    let result: usize = 0;
+    let mids = valid_updates
+        .iter()
+        .map(|u| u.get(u.len() / 2).unwrap());
+
+    let result: u32 = mids.sum();
     return result;
 }
 
 pub fn main() {
     let sample = include_str!("day05.sample");
-    validate::eq(0, solve(sample), "day05 part01 sample");
+    validate::eq(143, solve(sample), "day05 part01 sample");
 
-    // let input = include_str!("day05.input");
-    // validate::eq(0, solve(input), "day05 part01 input");
+    let input = include_str!("day05.input");
+    validate::eq(5268, solve(input), "day05 part01 input");
 }
--- adventofcode.com/2024/src/day05_part02.rs
+++ adventofcode.com/2024/src/day05_part02.rs
@@ -1,14 +1,104 @@
 use super::validate;
 
-fn solve(_input: &str) -> usize {
-    let result: usize = 0;
+use itertools::Itertools;
+use nom::{
+    bytes::complete::tag, character::complete::{self, line_ending}, multi::separated_list1, IResult
+};
+
+fn rules(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
+    separated_list1(line_ending, 
+        separated_list1(tag("|"), complete::u32),
+    )(input)
+}
+
+fn updates(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
+    separated_list1(line_ending, 
+        separated_list1(tag(","), complete::u32),
+    )(input)
+}
+
+fn parse(input: &str) -> IResult<&str, (Vec<Vec<u32>>, Vec<Vec<u32>>)> {
+    let (input, parsed_rules) = rules(input)?;
+    let (input, parsed_updates) = updates(input.trim_start())?;
+
+    Ok((input, (parsed_rules, parsed_updates)))
+}
+
+fn valid_update(rules: &Vec<Vec<u32>>, update: &Vec<u32>) -> bool {
+    let invalid = rules
+        .iter()
+        .any(|rule| {
+            let l = rule.get(0).unwrap();
+            let r = rule.get(1).unwrap();
+
+            if !update.contains(l) || !update.contains(r) {
+                return false;
+            }
+
+            let left_index = update.iter().position(|v| v == l).unwrap();
+            let right_index = update.iter().position(|v| v == r).unwrap();
+
+            return left_index >= right_index;
+        });
+
+    return !invalid;
+}
+
+fn correct_update(rules: &Vec<Vec<u32>>, update: &Vec<u32>) -> Vec<u32> {
+    let mut index = 0;
+
+    let mut u = update.to_vec();
+    while index < rules.len() {
+        let l = rules.get(index).unwrap().get(0).unwrap();
+        let r = rules.get(index).unwrap().get(1).unwrap();
+
+        if !u.contains(l) || !u.contains(r) {
+            index += 1;
+            continue;
+        }
+
+        let left_index = u.iter().position(|v| v == l).unwrap();
+        let right_index = u.iter().position(|v| v == r).unwrap();
+
+        if left_index >= right_index {
+            let ll = u[left_index];
+            let rr = u[right_index];
+
+            u[right_index] = ll;
+            u[left_index] = rr;
+
+            index = 0;
+        } else {
+            index += 1;
+        }
+    }
+
+    return u;
+
+}
+
+fn solve(input: &str) -> u32 {
+
+    let (_input, (parsed_rules, parsed_updates)) = parse(input).unwrap();
+
+    let valid_updates = parsed_updates
+        .iter()
+        .filter(|u| valid_update(&parsed_rules, &u) == false)
+        .map(|u| correct_update(&parsed_rules, &u))
+        .collect_vec();
+
+    let mids = valid_updates
+        .iter()
+        .map(|u| u.get(u.len() / 2).unwrap());
+
+    let result: u32 = mids.sum();
     return result;
 }
 
 pub fn main() {
     let sample = include_str!("day05.sample");
-    validate::eq(0, solve(sample), "day05 part02 sample");
+    validate::eq(123, solve(sample), "day05 part02 sample");
 
     let input = include_str!("day05.input");
-    validate::eq(0, solve(input), "day05 part02 input");
+    validate::eq(5799, solve(input), "day05 part02 input");
 }
