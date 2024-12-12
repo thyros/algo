8f97daff1eaa796ccf9f10d8f00108c29a34d070
Thyros <thyros@gmail.com>
2024-12-12 22:07:44 +0000

    start day 7

--- adventofcode.com/2024/src/main.rs
+++ adventofcode.com/2024/src/main.rs
@@ -1,29 +1,9 @@
 mod validate;
 
-mod day01_part01;
-mod day01_part02;
-mod day02_part01;
-mod day02_part02;
-mod day03_part01;
-mod day03_part02;
-mod day04_part01;
-mod day04_part02;
-mod day05_part01;
-mod day05_part02;
-mod day06_part01;
-mod day06_part02;
+mod day07_part01;
+mod day07_part02;
 
 fn main() {
-    day01_part01::main();
-    day01_part02::main();
-    day02_part01::main();
-    day02_part02::main();
-    day03_part01::main();
-    day03_part02::main();
-    day04_part01::main();
-    day04_part02::main();
-    day05_part01::main();
-    day05_part02::main();
-    day06_part01::main();
-    day06_part02::main();
+    day07_part01::main();
+    day07_part02::main();
 }
