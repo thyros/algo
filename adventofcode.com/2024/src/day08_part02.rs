9bdea5f1c39fe91d14c918f4d18f66897687796c
Thyros <thyros@gmail.com>
2024-12-19 00:12:07 +0000

    day 8

--- adventofcode.com/2024/src/day08_part02.rs
+++ adventofcode.com/2024/src/day08_part02.rs
@@ -1,14 +1,87 @@
-use super::validate;
+use std::iter::successors;
 
-fn solve(_input: &str) -> usize {
-    let result: usize = 0;
-    return result;
+use super::validate;
+use glam::IVec2;
+use itertools::Itertools;
+use nom::{
+    bytes::complete::take_till, character::complete::satisfy, multi::many1, sequence::preceded,
+    IResult, AsChar,
+};
+use nom_locate::{position, LocatedSpan};
+
+pub type Span<'a> = LocatedSpan<&'a str>;
+
+fn alphanum_pos(input: Span) -> IResult<Span, (IVec2, char)> {
+    let (input, pos) = position(input)?;
+    let x = pos.get_column() as i32 - 1;
+    let y = pos.location_line() as i32 - 1;
+    let (input, c) = satisfy(|c| c.is_alphanum())(input)?;
+    Ok((input, (IVec2::new(x, y), c)))
+}
+pub fn parse(input: Span) -> IResult<Span, Vec<(IVec2, char)>> {
+    many1(preceded(take_till(|c: char| c.is_alphanum()), alphanum_pos))(input)
+}
+
+fn solve(input: &str) -> usize {
+    let height = input.lines().count();
+    let width = input.lines().next().unwrap().len();
+
+    let x_bound = 0i32..width as i32;
+    let y_bound = 0i32..height as i32;
+
+    let (_input, mut result) = parse(Span::new(input)).unwrap();
+
+    result.sort_by(|a, b| a.1.cmp(&b.1));
+    let results = result
+        .chunk_by(|a, b| a.1 == b.1)
+        .flat_map(|chunk| {
+            chunk.iter()
+            .combinations(2)
+            .flat_map(|sats| {
+                let diff = sats[0].0 - sats[1].0;
+
+                let left_results: Vec<_> = successors(
+                    Some(sats[0].0),
+                    |pos| {
+                        let new_pos = pos + diff;
+                        if x_bound.contains(&new_pos.x) && y_bound.contains(&new_pos.y) {
+                            Some(new_pos)
+                        } else {
+                            None
+                        }
+                    },
+                ).collect();
+
+                let right_results: Vec<_> = successors(
+                    Some(sats[1].0),
+                    |pos| {
+                        let new_pos = pos - diff;
+                        if x_bound.contains(&new_pos.x) && y_bound.contains(&new_pos.y) {
+                            Some(new_pos)
+                        } else {
+                            None
+                        }
+                    },
+                ).collect();
+
+
+                [left_results, right_results]
+            })
+            .flatten()
+        })
+        .filter(|pos|{
+            x_bound.contains(&pos.x) && y_bound.contains(&pos.y)
+        })
+        .unique()
+        .count();
+
+    return results;
 }
 
 pub fn main() {
     let sample = include_str!("day08.sample");
-    validate::eq(0, solve(sample), "day08 part02 sample");
+    validate::eq(34, solve(sample), "day08 part02 sample");
 
     let input = include_str!("day08.input");
-    validate::eq(0, solve(input), "day08 part02 input");
+    validate::eq(1229, solve(input), "day08 part02 input");
 }
