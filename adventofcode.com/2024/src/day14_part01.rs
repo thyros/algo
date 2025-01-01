ca4d535a4f2cbd13cf537ed25d2b8b8c8d7bc5de
Thyros <thyros@gmail.com>
2025-01-01 19:31:02 +0000

    starting day 14

---
+++ adventofcode.com/2024/src/day14_part01.rs
@@ -0,0 +1,14 @@
+use super::validate;
+
+fn solve(_input: &str) -> usize {
+    let result: usize = 0;
+    return result;
+}
+
+pub fn main() {
+    let sample = include_str!("day14.sample").trim();
+    validate::eq(0, solve(sample), "day14 part01 sample");
+
+    let input = include_str!("day14.input").trim();
+    validate::eq(0, solve(input), "day14 part01 input");
+}
--- adventofcode.com/2024/src/main.rs
+++ adventofcode.com/2024/src/main.rs
@@ -1,9 +1,9 @@
 mod validate;
 
-mod day13_part01;
-mod day13_part02;
+mod day14_part01;
+mod day14_part02;
 
 fn main() {
-    // day13_part01::main();
-    day13_part02::main();
+    day14_part01::main();
+    day14_part02::main();
 }
