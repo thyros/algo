b678a9a67a7428b30ca34c3851ca799162d0dde0
Thyros <thyros@gmail.com>
2025-01-03 14:56:15 +0000

    day 15

--- adventofcode.com/2024/src/day15_part01.rs
+++ adventofcode.com/2024/src/day15_part01.rs
@@ -1,14 +1,139 @@
-use super::validate;
+use std::collections::HashSet;
 
-fn solve(_input: &str) -> usize {
-    let result: usize = 0;
+use glam::IVec2;
+use itertools::Itertools;
+
+use super::validate;
+#[derive(Debug)]
+struct Warehouse {
+    walls: HashSet<IVec2>,
+    boxes: HashSet<IVec2>,
+    robot: IVec2,
+    size: IVec2,
+}
+
+fn parse_warehouse(input: &str) -> Warehouse {
+    let mut walls: HashSet<IVec2> = HashSet::default();
+    let mut boxes: HashSet<IVec2> = HashSet::default();
+    let mut robot: IVec2 = IVec2::ZERO;
+
+    let lines = input.lines().collect_vec();
+    let size: IVec2 = IVec2::new(lines.len() as i32, lines[0].len() as i32);
+    
+    for (y, line) in input.lines().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            let p = IVec2::new(x as i32, y as i32);
+            match c {
+                '#' => {walls.insert(p);}
+                'O' => {boxes.insert(p);}
+                '@' => {robot = p; }
+                _ => {}
+            }
+        }
+    }
+
+    return Warehouse{ walls, boxes, robot, size}; 
+}
+
+fn parse_commands(input: &str) -> String {
+    input.replace("\r\n", "")
+}
+
+fn get_obj (pos: &IVec2, walls: &HashSet<IVec2>, size: &IVec2, boxes: &HashSet<IVec2>, robot: &IVec2) -> char {
+    if pos.x < 0 || pos.y < 0 || pos.x >= size.x || pos.y >= size.y {
+        return '#';
+    } else if walls.contains(pos) {
+        return '#';
+    } else if boxes.contains(pos) {
+        return 'O';
+    } else if robot == pos {
+        return '@'
+    }
+    return '.';
+}
+
+fn _print_warehouse(walls: &HashSet<IVec2>, size: &IVec2, boxes: &HashSet<IVec2>, robot: &IVec2) {
+    for y in 0..size.y {
+        for x in 0..size.x {
+            print!("{}", get_obj(&IVec2::new(x, y), walls, size, boxes, robot));            
+        }
+        println!();
+    }
+}
+
+fn push(walls: &HashSet<IVec2>, size: &IVec2, boxes: &mut HashSet<IVec2>, robot: &mut IVec2, dir: IVec2) {
+    let a_pos = *robot + dir;
+    if get_obj(&a_pos, &walls, &size, &boxes, &robot) == '.' {
+        *robot = a_pos;
+        return;
+    }
+
+    let mut b_pos = a_pos;
+    loop {
+        let obj = get_obj(&b_pos, &walls, &size, &boxes, &robot);
+        match obj {
+            '#' => { break; }
+            'O' => { b_pos += dir; }
+            '.' => {
+                boxes.insert(b_pos);
+                boxes.remove(&a_pos);
+                *robot = a_pos;
+                break;
+            }
+            _ => { panic!("Unrecognized command"); }
+        }
+    }
+}
+
+fn simulate(walls: &HashSet<IVec2>, size: &IVec2, mut boxes: HashSet<IVec2>, mut robot: IVec2, commands: &str) -> HashSet<IVec2> {
+    // print_warehouse(&walls, &size, &boxes, &robot);
+    for c in commands.chars() {
+        // println!("Command: {}", c);
+        match c {
+            '<' => { push(&walls, &size, &mut boxes, &mut robot, IVec2::new(-1, 0)); }
+            '>' => { push(&walls, &size, &mut boxes, &mut robot, IVec2::new(1, 0)); }
+            '^' => { push(&walls, &size, &mut boxes, &mut robot, IVec2::new(0, -1)); }
+            'v' => { push(&walls, &size, &mut boxes, &mut robot, IVec2::new(0, 1)); }
+            _ => { panic!("Unrecongnized command"); }
+        }
+        // print_warehouse(&walls, &size, &boxes, &robot);
+    }
+
+    return boxes;
+}
+
+fn parse(input: &str) -> (Warehouse, String) {
+
+    let parts = input.split("\r\n\r\n").collect_vec();
+    let warehouse = parse_warehouse(parts[0]);
+    let commands = parse_commands(parts[1]);
+
+    return (warehouse, commands);
+}
+
+fn count_result(boxes: HashSet<IVec2>)  -> i32 {
+    boxes.iter().map(|b| {
+        return 100 * b.y + b.x;
+    }).sum()
+}
+
+fn solve(input: &str) -> i32 {
+    let (warehouse, commands) = parse(input);
+    // println!("{:?}\n{:?}", &warehouse, &commands);
+
+    let boxes = simulate(&warehouse.walls, &warehouse.size, warehouse.boxes, warehouse.robot, &commands);
+
+    let result = count_result(boxes);
     return result;
 }
 
 pub fn main() {
-    let sample = include_str!("day15.sample").trim();
-    validate::eq(0, solve(sample), "day15 part01 sample");
+    let sample = include_str!("day15_1.sample").trim();
+    validate::eq(2028, solve(sample), "day15 part01 sample 1");
+
+    let sample = include_str!("day15_2.sample").trim();
+    validate::eq(10092, solve(sample), "day15 part01 sample 2");
 
     let input = include_str!("day15.input").trim();
-    validate::eq(0, solve(input), "day15 part01 input");
+    validate::eq(1487337, solve(input), "day15 part01 input");
 }
--- adventofcode.com/2024/src/day15_part02.rs
+++ adventofcode.com/2024/src/day15_part02.rs
@@ -1,14 +1,235 @@
-use super::validate;
+use std::collections::{HashMap, HashSet};
 
-fn solve(_input: &str) -> usize {
-    let result: usize = 0;
+use glam::IVec2;
+use itertools::Itertools;
+
+use super::validate;
+#[derive(Debug)]
+struct Warehouse {
+    grid: Vec<Vec<char>>,
+    robot: IVec2,
+}
+
+fn parse_warehouse(input: &str) -> Warehouse {
+    let mut grid: Vec<Vec<char>> = Vec::default();
+    let mut robot: IVec2 = IVec2::ZERO;
+
+    for (y, line) in input.lines().enumerate() {
+        let mut row: Vec<char> = Vec::default();
+
+        for (x, c) in line.chars().enumerate() {
+            match c {
+                '#' => {
+                    row.extend(['#', '#']);
+                }
+                'O' => {
+                    row.extend(['[', ']']);
+                }
+                '@' => {
+                    row.extend(['.', '.']);
+                    robot = IVec2::new((x * 2) as i32, y as i32);
+                }
+                _ => {
+                    row.extend(['.', '.']);
+                }
+            }
+        }
+
+        grid.push(row);
+    }
+
+    return Warehouse { grid, robot };
+}
+
+fn parse_commands(input: &str) -> String {
+    input.replace("\r\n", "")
+}
+
+fn _print_warehouse(warehouse: &Warehouse) {
+    for (y, row) in warehouse.grid.iter().enumerate() {
+        for (x, c) in row.iter().enumerate() {
+            if warehouse.robot.x == x as i32 && warehouse.robot.y == y as i32 {
+                print!("@");
+            } else {
+                print!("{}", c);
+            }
+        }
+        println!();
+    }
+}
+
+fn push_horizontal(warehouse: &mut Warehouse, dir: IVec2) {
+    let a_pos = warehouse.robot + dir;
+
+    if warehouse.grid[a_pos.y as usize][a_pos.x as usize] == '.' {
+        warehouse.robot = a_pos;
+        return;
+    }
+
+    let mut b_pos = a_pos;
+    loop {
+        let obj = warehouse.grid[b_pos.y as usize][b_pos.x as usize];
+        match obj {
+            '#' => {
+                break;
+            }
+            '[' => {
+                b_pos += dir;
+            }
+            ']' => {
+                b_pos += dir;
+            }
+            '.' => {
+                while b_pos.x != a_pos.x {
+                    warehouse.grid[b_pos.y as usize][b_pos.x as usize] =
+                        warehouse.grid[b_pos.y as usize][(b_pos.x - dir.x) as usize];
+                    b_pos.x -= dir.x;
+                }
+
+                warehouse.grid[a_pos.y as usize][a_pos.x as usize] = '.';
+                warehouse.robot = a_pos;
+                break;
+            }
+            _ => {
+                panic!("Unrecognized command")
+            }
+        }
+    }
+}
+
+fn can_push(
+    warehouse: &Warehouse,
+    pos: IVec2,
+    dir: IVec2,
+    checked: &mut HashMap<IVec2, bool>,
+) -> bool {
+    let c = warehouse.grid[pos.y as usize][pos.x as usize];
+    if c == '.' {
+        return true;
+    } else if c == '#' {
+        return false;
+    }
+
+    if checked.contains_key(&pos) {
+        return *checked.get(&pos).unwrap();
+    }
+
+    let a_pos = pos + dir;
+    if c == '[' {
+        let can_push = can_push(warehouse, a_pos, dir, checked)
+            && can_push(warehouse, a_pos + IVec2::new(1, 0), dir, checked);
+        checked.insert(pos, can_push);
+        return can_push;
+    } else if c == ']' {
+        let can_push = can_push(warehouse, a_pos, dir, checked)
+            && can_push(warehouse, a_pos + IVec2::new(-1, 0), dir, checked);
+        checked.insert(pos, can_push);
+        return can_push;
+    }
+    return false;
+}
+
+fn push(warehouse: &mut Warehouse, pos: IVec2, dir: IVec2) {
+    // I'm probably pushing the same boxes more then once
+
+    let c = warehouse.grid[pos.y as usize][pos.x as usize];
+    let a_pos = pos + dir;
+    if c == '[' {
+        push(warehouse, a_pos, dir);
+        push(warehouse, a_pos + IVec2::new(1, 0), dir);
+        warehouse.grid[a_pos.y as usize][a_pos.x as usize] =
+            warehouse.grid[pos.y as usize][pos.x as usize];
+        warehouse.grid[a_pos.y as usize][(a_pos.x + 1) as usize] =
+            warehouse.grid[pos.y as usize][(pos.x + 1) as usize];
+        warehouse.grid[pos.y as usize][pos.x as usize] = '.';
+        warehouse.grid[pos.y as usize][(pos.x + 1) as usize] = '.';
+    } else if c == ']' {
+        push(warehouse, a_pos, dir);
+        push(warehouse, a_pos + IVec2::new(-1, 0), dir);
+        warehouse.grid[a_pos.y as usize][a_pos.x as usize] =
+            warehouse.grid[pos.y as usize][pos.x as usize];
+        warehouse.grid[a_pos.y as usize][(a_pos.x - 1) as usize] =
+            warehouse.grid[pos.y as usize][(pos.x - 1) as usize];
+        warehouse.grid[pos.y as usize][pos.x as usize] = '.';
+        warehouse.grid[pos.y as usize][(pos.x - 1) as usize] = '.';
+    }
+}
+
+fn push_vertially(warehouse: &mut Warehouse, dir: IVec2) {
+    let mut checked: HashMap<IVec2, bool> = HashMap::default();
+    let a_pos = warehouse.robot + dir;
+    if can_push(warehouse, a_pos, dir, &mut checked) {
+        push(warehouse, a_pos, dir);
+        warehouse.robot = a_pos;
+    }
+}
+
+fn simulate(mut warehouse: Warehouse, commands: &str) -> HashSet<IVec2> {
+    // print_warehouse(&warehouse);
+    for c in commands.chars() {
+        // println!("Command: {}", c);
+        match c {
+            '<' => {
+                push_horizontal(&mut warehouse, IVec2::new(-1, 0));
+            }
+            '>' => {
+                push_horizontal(&mut warehouse, IVec2::new(1, 0));
+            }
+            '^' => {
+                push_vertially(&mut warehouse, IVec2::new(0, -1));
+            }
+            'v' => {
+                push_vertially(&mut warehouse, IVec2::new(0, 1));
+            }
+            _ => {
+                panic!("Unrecongnized command");
+            }
+        }
+        // print_warehouse(&warehouse);
+    }
+
+    let mut boxes: HashSet<IVec2> = HashSet::default();
+    for (y, row) in warehouse.grid.iter().enumerate() {
+        for (x, c) in row.iter().enumerate() {
+            if c == &'[' {
+                boxes.insert(IVec2::new(x as i32, y as i32));
+            }
+        }
+    }
+    return boxes;
+}
+
+fn parse(input: &str) -> (Warehouse, String) {
+    let parts = input.split("\r\n\r\n").collect_vec();
+    let warehouse = parse_warehouse(parts[0]);
+    let commands = parse_commands(parts[1]);
+
+    return (warehouse, commands);
+}
+
+fn count_result(boxes: HashSet<IVec2>) -> i32 {
+    boxes
+        .iter()
+        .map(|b| {
+            return 100 * b.y + b.x;
+        })
+        .sum()
+}
+
+fn solve(input: &str) -> i32 {
+    let (warehouse, commands) = parse(input);
+    // println!("{:?}\n{:?}", &warehouse, &commands);
+
+    let boxes = simulate(warehouse, &commands);
+
+    let result = count_result(boxes);
     return result;
 }
 
 pub fn main() {
-    let sample = include_str!("day15.sample").trim();
-    validate::eq(0, solve(sample), "day15 part02 sample");
+    let sample = include_str!("day15_2.sample").trim();
+    validate::eq(9021, solve(sample), "day15 part02 sample");
 
     let input = include_str!("day15.input").trim();
-    validate::eq(0, solve(input), "day15 part02 input");
+    validate::eq(1521952, solve(input), "day15 part02 input");
 }
